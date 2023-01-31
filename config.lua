e030bb8d8bbbba4436e6b61ac92b317301df8457
Babis Chalios <mail@bchalios.io>
2023-01-31 14:36:52 +0000

    Use relative line numbers
    Signed-off-by: Babis Chalios <mail@bchalios.io>

--- config.lua
+++ config.lua
@@ -15,6 +15,9 @@ lvim.colorscheme = "gruvbox"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
+-- use relative line numbers
+vim.opt.relativenumber = true
+
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
